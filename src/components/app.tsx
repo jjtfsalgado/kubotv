77ed6cc7d2b6d9563db22fba37d785def95d7e3c
jjtfsalgado <jjtfsalgado@gmail.com>
2019-05-01 20:49:05 +0100

    First commit

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,9 +1,32 @@
 import * as React from 'react';
 import css from "./app.less";
+// import Hls from "../../dist/index";
 
-export const App = () =>
-    <div className={css.app}>
-        <h1>Hello from React and Typescrit!</h1>
-    </div>;
+
+export class App extends React.Component<{},{}>{
+    render() {
+        document
+
+        //@ts-ignore
+        if(window.Hls){
+            console.log("hello hls.js!");
+
+        }
+        // if (Hls.isSupported()) {
+        // }
+
+
+
+
+        return (
+            <div className={css.app}>
+                <h1>Hello from React and Typescrit!</h1>
+            </div>
+        )
+    }
+}
+
+
+
 
 
