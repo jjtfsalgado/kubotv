93b374a87fa8fcb9be1a1db0e803c4283ba7826e
José Salgado <jjtfsalgado@gmail.com>
2019-12-12 20:48:45 +0000

    updated server scripts

--- server.js
+++ server.js
@@ -13,8 +13,6 @@ app.use(function (req, res, next){
     }
 });
 
-// app.use(express.static('dist'));
-
 app.use(express.static(path.join(__dirname, 'dist')));
 
 app.get('*', (req, res) => res.sendFile(path.join(__dirname, 'dist/index.html')));
