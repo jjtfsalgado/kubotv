d3d02782e06a85d6078a911d165fd223f008b5f5
José Salgado <jjtfsalgado@gmail.com>
2019-12-16 21:18:12 +0000

    Update index.ts

--- src/server/index.ts
+++ src/server/index.ts
@@ -13,7 +13,7 @@ app.use(function (req, res, next){
     }
 });
 
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static('dist'));
 
 app.get('*', (req, res) => res.sendFile(path.join(__dirname, 'dist/index.html')));
 
