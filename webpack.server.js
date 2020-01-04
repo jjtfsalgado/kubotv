2f1456c7dc963318fcd69766e8927f377cb77f52
José Salgado <jjtfsalgado@gmail.com>
2020-01-04 20:19:06 +0000

    Fixed gitignore issue

---
+++ webpack.server.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const NodemonPlugin = require('nodemon-webpack-plugin');
+const webpack = require('webpack');
+
+module.exports = {
+    entry: [
+        './src/server/index.ts'
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.(ts|tsx)$/,
+                loader: 'awesome-typescript-loader',
+                exclude: /node_modules/
+            }
+        ]
+    },
+    devtool: 'source-map',
+    resolve: {
+        extensions: [ '.tsx', '.ts', '.js', '.jsx' ]
+    },
+    plugins: [
+        new NodemonPlugin()
+    ],
+    target: 'node',
+    node: {
+        __dirname: false
+    },
+    output: {
+        filename: "server.js",
+        path: __dirname
+    },
+};
