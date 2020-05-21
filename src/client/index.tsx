718a120f0fe73885cfd3fe87088b639eef055937
José Salgado <jjtfsalgado@gmail.com>
2020-05-21 18:20:47 +0100

    render on window onLoad

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -1,18 +1,24 @@
 import * as React from 'react';
 import {render} from 'react-dom';
 import {App} from "./app/app";
-import { Provider } from 'react-redux'
+import {Provider} from 'react-redux'
 import {store} from "./reducers";
 import {HashRouter} from "react-router-dom";
 
-render(
-    <Provider store={store}>
-        <HashRouter>
-            <App/>
-        </HashRouter>
-    </Provider>,
-    document.getElementById('root')
-);
+
+const renderLoad = () => {
+    render(
+        <Provider store={store}>
+            <HashRouter>
+                <App/>
+            </HashRouter>
+        </Provider>,
+        document.getElementById('root')
+    );
+};
+
+window.onload = renderLoad;
+
 
 // if ('serviceWorker' in navigator) {
 //     window.addEventListener('load', function() {
