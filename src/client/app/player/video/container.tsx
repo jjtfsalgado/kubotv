b6285584e0fddd65a333c5fcc6d1afb92c73b5d3
José Salgado <jjtfsalgado@gmail.com>
2020-06-15 11:42:26 +0100

    Fixed fit content mozilla issue

--- src/client/app/player/video/container.tsx
+++ src/client/app/player/video/container.tsx
@@ -1,10 +1,10 @@
 import * as React from "react";
 import {cls} from "../../../../utils/function";
-import {Video} from "./video";
 import {useSelector} from "react-redux";
 import {IRootState} from "../../../reducers";
 import {IChannel} from "../../../controllers/playerCtrl";
 import css from "./video.less";
+import {Video} from "./video";
 
 interface IVideoProps {
     showControls?: boolean;
